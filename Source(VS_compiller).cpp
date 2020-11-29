f98048d8325a7070c22617f2118b7617d62d2eb5
MAGGen-hub <MAGGen-hub@users.noreply.github.com>
2020-11-29 23:57:56 +0300

    Create project
    Remade report, Sorce files, tests and screenshots for task 4. Add program a(c++ compilled).exe complied with: c++ Source(c++_compiller).cpp -fopenmp

---
+++ Source(VS_compiller).cpp
@@ -0,0 +1,104 @@
+/*
+Исайков Иван Александрович
+БПИ 193-2
+Вариант №7
+Потоки
+Вычислить прямое произведение множеств А1, А2, А3… Аn.
+Входные данные: целое положительное число n, множества чисел А1, А2,
+А3… Аn, мощности множеств равны между собой и мощность каждого
+множества больше или равна 1. Количество потоков является входным
+параметром.
+
+Входные параметры: 1 - колво потоков 2 - колво файлов 3 - путь к входным файлам с множествами
+В случае отклонения от условия задачи работоспособность программы не гарантируется...
+*/
+
+// c++ main.cpp -fopenmp //for correct omp work
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <omp.h>
+using namespace std;
+
+fstream* fileThreads;
+
+
+int main(int argc, char** argv)
+{
+	try{
+		string fileway = argv[3];//full way to file without extention and intdex (../int/*Filename**index*.*extend*)
+		string fileextention = ".txt";
+		
+
+		int n = atoi(((string)argv[1]).c_str());
+		int numberOfThreads = atoi(((string)argv[2]).c_str());
+		fileThreads = new fstream[n];
+
+		#pragma omp parallel num_threads(numberOfThreads)
+		{
+			bool check = true;
+			#pragma omp for
+			for (int i = 0; i < n; i++)//files init
+			{
+
+				auto num = omp_get_thread_num();
+				//#pragma omp critical //for test
+				//{
+				//	std::cout << " thread " << num << std::endl;
+				//}
+				char buffer[100];
+				string str = fileway;
+				_itoa(i + 1, buffer, 10);//возможны проблемы при компиляции с помощью с++ Source.cpp -fopenmp //при возникновении заменить на _itoa_s(i + 1, buffer, 10);
+				str.append(buffer);
+				str.append(fileextention);
+				fileThreads[i] = fstream(str, ios::in);
+
+				if (!fileThreads[i].is_open())
+					check = false;
+			}
+			if (!check)
+				throw runtime_error("Error! Failed to open input file!");
+		}
+
+		omp_set_num_threads(numberOfThreads);
+		#pragma omp parallel
+		{
+			do
+			{
+				char buffer[100];
+				string rezult = "(";
+				int* num = new int[n];
+				bool temp = true;
+				//чтение из файловых потоков
+				#pragma omp critical (nm)
+				{
+					for (int j = 0; j < n; j++)
+					{	
+						if (fileThreads[j].eof())
+							temp = false;
+						fileThreads[j] >> num[j];
+					}
+				}
+				for (int j = 0; j < n; j++)
+				{
+					_itoa(num[j], buffer, 10);//возможны проблемы при компиляции с помощью с++ Source.cpp -fopenmp //при возникновении заменить на _itoa_s(num[j], buffer, 10);
+					rezult.append(buffer);
+
+					if (j + 1 < n)
+						rezult.append(", ");
+				}
+				rezult.append(")");//конец элемента прямого (декартового) произведения
+				#pragma omp critical (cout)
+				{
+					if(temp)
+					cout << omp_get_thread_num() << ": " << rezult << endl;
+				}
+			} while (!fileThreads[0].eof());
+		}
+	}
+	catch (exception ex)
+	{
+		cout << ex.what() << endl;
+	}
+	return 0;//завершение
+}
